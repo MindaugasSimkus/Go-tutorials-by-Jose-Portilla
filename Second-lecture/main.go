9988d66ca695d0548057ac96e379e8f0541127e4
Mindaugas <shimkus.mindaugas@gmail.com>
2023-05-22 20:48:33 +0300

    Update main.go

--- Second-lecture/main.go
+++ Second-lecture/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+//import "fmt"
+
+//func main() {
+//	var speed int
+//	fmt.Println(speed)
+//}
+
+//third lecture - Path separator
+
+import (
+	"fmt"
+	"path"
+)
 
 func main() {
-	var speed int
-	fmt.Println(speed)
+	var dir, file string
+	dir, file = path.Split("css/main.css")
+	fmt.Println("dir :", dir)
+	fmt.Println("file :", file)
+
+	var file2 string
+	_, file2 = path.Split("css2/main2.css")
+	fmt.Println("file2 :", file2)
+
+	//short declaration
+
+	dir3, _ := path.Split("css3/main3.css")
+	fmt.Println("dir3:", dir3)
+
 }
